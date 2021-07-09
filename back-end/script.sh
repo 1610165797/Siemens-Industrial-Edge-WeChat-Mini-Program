7648c1c22481ce1c4eb670e08afb51ad7c75784b
1610165797 <77814631+1610165797@users.noreply.github.com>
2021-07-09 16:07:48 +0800

    Delete back-end directory

--- back-end/script.sh
+++
@@ -1,9 +0,0 @@
-SIGTERM
-clear
-gnome-terminal -e "python3 sendMessage.py"
-gnome-terminal -e "python3 sankey_sub.py"
-sleep 2s
-gnome-terminal -e "python3 sankey_pub.py"
-gnome-terminal -e "python3 line_pub.py"
-sleep 2s
-gnome-terminal -e "python3 line_sub.py"
